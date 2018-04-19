1018b8efb3a1088d25262915deada544977e6d41
Chen Zu <chen0zu@gmail.com>
2018-04-20 08:40:38 +1000

    update

--- 04-NeuroNetworks/main.m
+++ 04-NeuroNetworks/main.m
@@ -1,5 +1,15 @@
 clc;
 clear;
+%%
+n = 100; d = 28;
+X = rand(n,d);
+n_input = d; n_hidden = 16; n_output = 3;
+W1 = zeros(n_hidden,n_input);
+b1 = zeros(n_hidden,1);
+W2 = zeros(n_output,n_hidden);
+b2 = zeros(n_output,1);
+
+%%
 
 function [f,g] = loss()
 end
