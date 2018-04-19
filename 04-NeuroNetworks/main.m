a508043f6b80d8d00c391e6024392262a489c3ab
Chen Zu <chen0zu@gmail.com>
2018-04-20 08:59:49 +1000

    update

--- 04-NeuroNetworks/main.m
+++ 04-NeuroNetworks/main.m
@@ -1,15 +1,30 @@
 clc;
 clear;
+dbstop if error
 %%
-n = 100; d = 28;
+n = 100; d = 4;
 X = rand(n,d);
-n_input = d; n_hidden = 16; n_output = 3;
-W1 = zeros(n_hidden,n_input);
-b1 = zeros(n_hidden,1);
-W2 = zeros(n_output,n_hidden);
-b2 = zeros(n_output,1);
+n_input = d; n_hidden = 3; n_output = 2;
+W1 = randn(n_hidden,n_input);
+b1 = randn(n_hidden,1);
+W2 = randn(n_output,n_hidden);
+b2 = randn(n_output,1);
+
+theta = [W1(:);b1;W2(:);b2];
+
+[Z1,z1,Z2,z2] = unfold(theta, n_input, n_hidden, n_output);
 
 %%
+
+%%
+function [W1,b1,W2,b2] = unfold(theta, n_input, n_hidden, n_output)
+W1 = reshape(theta(1:n_hidden*n_input), n_hidden, n_input);
+b1 = theta(n_hidden*n_input+1:n_hidden*n_input+n_hidden);
+W2 = reshape(theta(n_hidden*n_input+n_hidden+1:...
+    n_hidden*n_input+n_hidden+n_output*n_hidden), n_output, n_hidden);
+b2 = theta(n_hidden*n_input+n_hidden+n_output*n_hidden+1:...
+    n_hidden*n_input+n_hidden+n_output*n_hidden+n_output);
+end
 
 function [f,g] = loss()
 end
