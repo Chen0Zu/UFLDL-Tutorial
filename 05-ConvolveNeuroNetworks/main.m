c1a3c2620902ba0ec953d5004b037368fb923766
Chen Zu <chen0zu@gmail.com>
2018-04-23 09:05:06 +1000

    update

--- 05-ConvolveNeuroNetworks/main.m
+++ 05-ConvolveNeuroNetworks/main.m
@@ -3,10 +3,14 @@ addpath(genpath('../minFunc_2012'));
 addpath('../common');
 rng(1);
 
+test = [1 1 1 0 0; 0 1 1 1 0; 0 0 1 1 1; 0 0 1 1 0; 0 1 1 0 0];
+W = [1 0 1; 0 1 0; 1 0 1];
+f = cnnConvolve(test,W);
+
 %%
 
 function f = cnnConvolve(image, W)
 % Flip W for use in conv2
 W = rot90(W,2);
-f = conv2(image,W);
+f = conv2(image,W,'valid');
 end
