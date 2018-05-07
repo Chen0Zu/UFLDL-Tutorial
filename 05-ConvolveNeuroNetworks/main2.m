a97847e0890d3711e3e032988d0590b4a1af008f
Chen Zu <chen0zu@gmail.com>
2018-05-07 22:09:34 +1000

    update

--- 05-ConvolveNeuroNetworks/main2.m
+++ 05-ConvolveNeuroNetworks/main2.m
@@ -13,6 +13,8 @@
 %  Here we initialize some parameters used for the exercise.
 clc;clear;close all;
 dbstop if error;
+addpath(genpath('../minFunc_2012'));
+addpath('../common');
 rng('default');
 
 % Configuration
@@ -31,3 +33,43 @@ labels(labels==0) = 10; % Remap 0 to 10
 
 % Initialize Parameters
 theta = cnnInitParams(imageDim,filterDim,numFilters,poolDim,numClasses);
+
+%%======================================================================
+%% STEP 2: Gradient Check
+%  Use the file computeNumericalGradient.m to check the gradient
+%  calculation for your cnnCost.m function.  You may need to add the
+%  appropriate path or copy the file to this directory.
+
+DEBUG=false;  % set this to true to check gradient
+if DEBUG
+    % To speed up gradient checking, we will use a reduced network and
+    % a debugging data set
+    db_numFilters = 2;
+    db_filterDim = 9;
+    db_poolDim = 5;
+    db_images = images(:,:,1:15);
+    db_labels = labels(1:15);
+    db_theta = cnnInitParams(imageDim,db_filterDim,db_numFilters,...
+                db_poolDim,numClasses);
+    
+    [cost grad] = cnnCost(db_theta,db_images,db_labels,numClasses,...
+                                db_filterDim,db_numFilters,db_poolDim);
+    
+
+    % Check gradients
+    numGrad = computeNumericalGradient( @(x) cnnCost(x,db_images,...
+                                db_labels,numClasses,db_filterDim,...
+                                db_numFilters,db_poolDim), db_theta);
+ 
+    % Use this to visually compare the gradients side by side
+    disp([numGrad grad]);
+    
+    diff = norm(numGrad-grad)/norm(numGrad+grad);
+    % Should be small. In our implementation, these values are usually 
+    % less than 1e-9.
+    disp(diff); 
+ 
+    assert(diff < 1e-9,...
+        'Difference too large. Check your gradient computation again');
+    return;
+end
