6a28da2212c3968a4d125ecd48e8c6fe1745cf09
Chen Zu <chen0zu@gmail.com>
2018-05-07 23:16:57 +1000

    update

---
+++ 05-ConvolveNeuroNetworks/cnnPool.m
@@ -0,0 +1,46 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(imageRow, imageCol, featureNum, imageNum)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(poolRow, poolCol, featureNum, imageNum)
+%     
+
+numImages = size(convolvedFeatures, 4);
+numFilters = size(convolvedFeatures, 3);
+convolvedDim = size(convolvedFeatures, 1);
+
+pooledFeatures = zeros(convolvedDim / poolDim, ...
+        convolvedDim / poolDim, numFilters, numImages);
+
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   (convolvedDim/poolDim) x (convolvedDim/poolDim) x numFeatures x numImages 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(poolRow, poolCol, featureNum, imageNum) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region. 
+%   
+%   Use mean pooling here.
+
+%%% YOUR CODE HERE %%%
+for i = 1:numImages
+    for j = 1:numFilters
+        for row_idx = 1:convolvedDim/poolDim
+            for col_idx = 1:convolvedDim/poolDim
+                pool_area = convolvedFeatures(poolDim*row_idx-poolDim+1:poolDim*row_idx,...
+                    poolDim*col_idx-poolDim+1:poolDim*col_idx,j,i);
+                pooledFeatures(row_idx,col_idx,j,i) = mean(pool_area(:));
+            end
+        end
+    end
+end
+
+end
+
--- 05-ConvolveNeuroNetworks/cnnTrain.m
+++ 05-ConvolveNeuroNetworks/cnnTrain.m
@@ -73,3 +73,31 @@ if DEBUG
         'Difference too large. Check your gradient computation again');
     return;
 end
+
+%%======================================================================
+%% STEP 3: Learn Parameters
+%  Implement minFuncSGD.m, then train the model.
+
+options.epochs = 3;
+options.minibatch = 256;
+options.alpha = 1e-1;
+options.momentum = .95;
+tic;
+opttheta = minFuncSGD(@(x,y,z) cnnCost(x,y,z,numClasses,filterDim,...
+                      numFilters,poolDim),theta,images,labels,options);
+toc;
+%%======================================================================
+%% STEP 4: Test
+%  Test the performance of the trained model using the MNIST test set. Your
+%  accuracy should be above 97% after 3 epochs of training
+
+testImages = loadMNISTImages('../common/t10k-images-idx3-ubyte');
+testImages = reshape(testImages,imageDim,imageDim,[]);
+testLabels = loadMNISTLabels('../common/t10k-labels-idx1-ubyte');
+testLabels(testLabels==0) = 10; % Remap 0 to 10
+[~,cost,preds]=cnnCost(opttheta,testImages,testLabels,numClasses,...
+                filterDim,numFilters,poolDim,true);
+acc = sum(preds==testLabels)/length(preds);
+
+% Accuracy should be around 97.4% after 3 epochs
+fprintf('Accuracy is %f\n',acc);
